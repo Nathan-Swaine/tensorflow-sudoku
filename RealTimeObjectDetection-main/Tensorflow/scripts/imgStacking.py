c2932a3cd3c61ef3392708c3842ae0facbf0e0b5
Nathan Swaine <nathanswaine18@gmail.com>
2023-11-28 15:08:55 +0000

    classify contours in image

--- RealTimeObjectDetection-main/Tensorflow/scripts/imgStacking.py
+++ RealTimeObjectDetection-main/Tensorflow/scripts/imgStacking.py
@@ -1,5 +1,5 @@
 import cv2
-
+import sys
 def showImg(image):
   cv2.imshow("Original", image)
   cv2.waitKey(0)
@@ -14,12 +14,33 @@ def cnv2Thsh(image):
 def findContours(image):
   contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
   image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-  cv2.drawContours(image, contours, -1, (0,255,0), -1)
+  checkContour(image, contours)
   return image
+
+def checkContour(image, contours):
+  biggestContour = max(contours, key=cv2.contourArea)
+  cv2.drawContours(image, biggestContour, -1, (0,255,0), 15)
+  sides = len(cv2.approxPolyDP(biggestContour, 0.02 * cv2.arcLength(biggestContour, True), True))
+  if sides ==4: 
+    findPoints(biggestContour, image) 
+    #draw points on image
+    print(findPoints(biggestContour, image))
+    sys.exit()
+    return image
+  else: 
+    print("Contour not  a rectangle")  
+    sys.exit()
+
+def findPoints(bigCountur, image):
+  points = cv2.approxPolyDP(bigCountur, 0.02 * cv2.arcLength(bigCountur, True), True)
+  return points
+  
 
 image = cv2.imread("image_0.jpg")
 image = cnv2Thsh(image)
 image = findContours(image)
+
+
 showImg(image)
 
 
